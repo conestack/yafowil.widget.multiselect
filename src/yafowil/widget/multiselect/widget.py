4ce04da39df7d1e6cf36e77310ce8b7eb1156e0c
Robert Niederreiter <office@squarewave.at>
2015-10-05 13:59:42 +0200

    formatting

--- src/yafowil/widget/multiselect/widget.py
+++ src/yafowil/widget/multiselect/widget.py
@@ -1,10 +1,8 @@
 from yafowil.base import factory
-from yafowil.common import (
-    select_extractor,
-    generic_required_extractor,
-    select_edit_renderer,
-    select_display_renderer,
-)
+from yafowil.common import generic_required_extractor
+from yafowil.common import select_display_renderer
+from yafowil.common import select_edit_renderer
+from yafowil.common import select_extractor
 
 
 factory.register(
