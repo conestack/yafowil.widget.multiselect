a2eb6709f86045dc457a31a248829022e66a805b
Lena Daxenbichler <lena.daxenbichler@r2solutions.at>
2021-08-16 11:58:42 +0200

    setup karma and rollup

---
+++ src/yafowil/widget/multiselect/yafowil.widget.multiselect.js
@@ -0,0 +1,15 @@
+(function () {
+	'use strict';
+
+
+
+
+	if (window.yafowil === undefined) {
+	    window.yafowil = {};
+	}
+
+	window.yafowil.multiselect = exports;
+
+
+}());
+//# sourceMappingURL=yafowil.widget.multiselect.js.map
