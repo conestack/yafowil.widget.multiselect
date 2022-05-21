24ec0cc419f7753d12b7017e3840b6ab45dd88ec
Robert Niederreiter <rnix@squarewave.at>
2022-05-21 06:52:37 +0200

    "Register resources with webresource"

--- src/yafowil/widget/multiselect/__init__.py
+++ src/yafowil/widget/multiselect/__init__.py
@@ -1,9 +1,52 @@
 from yafowil.base import factory
 from yafowil.utils import entry_point
 import os
+import webresource as wr
 
 
-resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
+resources_dir = os.path.join(os.path.dirname(__file__), 'resources')
+
+
+##############################################################################
+# Default
+##############################################################################
+
+# webresource ################################################################
+
+scripts = wr.ResourceGroup(name='scripts')
+scripts.add(wr.ScriptResource(
+    name='multiselect-js',
+    depends='jquery-js',
+    directory=os.path.join(resources_dir, 'multi-select', 'js'),
+    resource='jquery.multi-select.js'
+))
+scripts.add(wr.ScriptResource(
+    name='yafowil-multiselect-js',
+    depends='multiselect-js',
+    directory=resources_dir,
+    resource='widget.js',
+    compressed='widget.min.js'
+))
+
+styles = wr.ResourceGroup(name='styles')
+styles.add(wr.StyleResource(
+    name='multiselect-css',
+    directory=os.path.join(resources_dir, 'multi-select', 'css'),
+    resource='multi-select.css'
+))
+styles.add(wr.StyleResource(
+    name='yafowil-multiselect-css',
+    depends='multiselect-css',
+    directory=resources_dir,
+    resource='widget.css'
+))
+
+resources = wr.ResourceGroup(name='multiselect-resources')
+resources.add(scripts)
+resources.add(styles)
+
+# B/C resources ##############################################################
+
 js = [{
     'group': 'yafowil.widget.multiselect.dependencies',
     'resource': 'multi-select/js/jquery.multi-select.js',
@@ -24,8 +67,16 @@ css = [{
 }]
 
 
+##############################################################################
+# Registration
+##############################################################################
+
 @entry_point(order=10)
 def register():
     from yafowil.widget.multiselect import widget  # noqa
-    factory.register_theme('default', 'yafowil.widget.multiselect',
-                           resourcedir, js=js, css=css)
+
+    # Default
+    factory.register_theme(
+        'default', 'yafowil.widget.multiselect', resources_dir,
+        js=js, css=css, resources=resources
+    )
